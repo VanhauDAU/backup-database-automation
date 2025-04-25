f585e807f9067f00b37150304174998ed3f084ca
VanhauDAU <levanhaum@gmail.com>
2025-04-25 21:41:58 +0700

    Cập nhật: thay đổi code

--- backup_script.py
+++ backup_script.py
@@ -22,11 +22,11 @@ if not os.path.exists(BACKUP_DIR):
     os.makedirs(BACKUP_DIR)
     print(f"Thư mục '{BACKUP_DIR}' đã được tạo.")
 
-# Email info
+# thông tin mail
 EMAIL_SENDER = os.getenv('EMAIL_SENDER')
 EMAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
 EMAIL_RECEIVER = os.getenv('EMAIL_RECEIVER')
-
+# hàm gửi mail
 def send_email(subject, content):
     msg = EmailMessage()
     msg.set_content(content)
@@ -41,7 +41,7 @@ def send_email(subject, content):
         print("Gửi mail thành công!.")
     except Exception as e:
         print("Gửi mail thất bại:", e)
-
+# hàm backup dữ liệu
 def backup_database():
     try:
         files_backed_up = []
@@ -61,12 +61,11 @@ def backup_database():
     except Exception as e:
         send_email("Backup thất bại", str(e))
 
-# Schedule chạy lúc 00:00 mỗi ngày
 schedule.every().day.at("00:00").do(backup_database)  
 
 print("Chương trình đang chạy và sẽ tự động backup lúc 00:00 mỗi ngày...")
 
-# Vòng lặp chạy liên tục
+# Vòng lặp chạy kiểm tra thời gian
 while True:
     schedule.run_pending()
     time.sleep(1)
